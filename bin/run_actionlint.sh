beee8eaa68235765c8a7e48a4feb093595049d39
nicomiguelino <nicomiguelino2014@gmail.com>
2025-02-09 21:33:58 -0800

    chore(ci): make use of the Dockerized version of `actionlint`

--- bin/run_actionlint.sh
+++ bin/run_actionlint.sh
@@ -3,4 +3,9 @@
 set -euo pipefail
 
 # Run actionlint on the workflows directory
-docker run --rm -v $(pwd):/repo --workdir /repo rhysd/actionlint:latest -color
+docker run \
+  --rm \
+  -v $(pwd):/repo \
+  --workdir /repo \
+  rhysd/actionlint:latest \
+  -color
