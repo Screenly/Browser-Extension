809c097a7a62af6cfe1895ac8e08b2e352fc5313
nicomiguelino <nicomiguelino2014@gmail.com>
2025-03-31 16:21:57 -0700

    chore: remove unused imports

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,6 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'development',
